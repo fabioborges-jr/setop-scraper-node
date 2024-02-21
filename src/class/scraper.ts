8d4a288776d35e9895728370db38d91e9a8c9944
fabioborges-jr <fabioborgesouzajunior@gmail.com>
2024-02-21 13:41:32 -0300

    :test_tube: test: Creates getRegions() prototype

--- src/class/scraper.ts
+++ src/class/scraper.ts
@@ -9,4 +9,10 @@ class scraper{
         this.page=await this.browser.newPage()
     }
 
+    async getRegions(){
+        if(typeof process.env.SETOP_URL==="string")
+        await this.page.goto(process.env.SETOP_URL)
+                .then(()=>this.page.waitForSelector("#component > div > div > div.span-16.content.last > ul > li:nth-child(1) > a"))
+                .then(()=>this.page.$$eval("div.span-16.content.last > ul > li > a", ((elements)=>elements.map(element=>console.log(element.textContent)))))
+    }
 }
